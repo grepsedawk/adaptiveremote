8850f3e94653e9c6b1ea0730c7f0d75a571dc442
Alex Piechowski <alex@piechowski.io>
2024-10-06 22:42:57 -0700

    Add basic layout and add roku remote controller via /remote

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var cookieParser = require("cookie-parser")
 var logger = require("morgan")
 
 var indexRouter = require("./routes/index")
-var usersRouter = require("./routes/users")
+var rokuRouter = require("./routes/roku")
+var remoteRouter = require("./routes/remote")
 
 var app = express()
 
@@ -20,7 +21,8 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, "public")))
 
 app.use("/", indexRouter)
-app.use("/users", usersRouter)
+app.use("/roku", rokuRouter)
+app.use("/remote", remoteRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -35,7 +37,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500)
-  res.render("error")
+  res.render("error", { title: "Error" })
 })
 
 module.exports = app
---
+++ routes/remote.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const router = express.Router()
+const Roku = require("../support/roku")
+
+router.get("/", async function (req, res, next) {
+  res.render("remote", { title: "Roku Remote" })
+  // TODO: remove keypress on get page load
+  const roku = new Roku("10.0.0.141")
+})
+
+router.get("/key/:key", async function (req, res, next) {
+  const roku = new Roku("10.0.0.141")
+  await roku.sendKey(req.params.key)
+
+  res.json({ status: "success" })
+})
+
+router.get("/launch/:app/:contentId/:mediaType", async function (req, res, next) {
+  const roku = new Roku("10.0.0.141")
+  await roku.launchApp(req.params.app, req.params.contentId, req.params.mediaType)
+
+  res.json({ status: "success" })
+})
+
+module.exports = router
