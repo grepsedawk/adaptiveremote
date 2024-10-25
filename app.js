a3ca88673582ca131e849f497528daad5260c062
José Alonso <jos.alonso@uc.cl>
2024-10-24 23:59:19 -0300

    Saving the remote

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var createError = require("http-errors")
-var express = require("express")
-var path = require("path")
-var cookieParser = require("cookie-parser")
-var logger = require("morgan")
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
 var indexRouter = require("./routes/index")
 var configureRouter = require("./routes/configure")
@@ -13,14 +13,14 @@ var JsonDB = require("./support/jsonDB")
 var app = express()
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"))
-app.set("view engine", "jade")
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger("dev"))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, "public")))
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter)
 app.use("/configure", configureRouter)
@@ -28,19 +28,19 @@ app.use("/remote", remoteRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404))
-})
+  next(createError(404));
+});
 
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get("env") === "development" ? err : {}
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  res.status(err.status || 500)
-  res.render("error", { title: "Error" })
-})
+  res.status(err.status || 500);
+  res.render("error", { title: "Error" });
+});
 
 app.locals.db = new JsonDB("db.json")
 
