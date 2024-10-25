1962080eed804f58725eb3af6d54c4004a310a63
José Antonio Alonso Schmitz <69523214+joalonsosch@users.noreply.github.com>
2024-10-25 02:20:46 -0300

    Style vertical remote 3 (#8)
    * Movie adding feature added
    
    * Movie adding and two color schemes
    
    * Saving the remote
    
    * Run prettier
    
    * Fix layout render
    
    ---------
    
    Co-authored-by: Alex Piechowski <alex@piechowski.io>

--- public/dynamics/remote.js
+++ public/dynamics/remote.js
@@ -1,16 +1,22 @@
-// Function to handle the toggle of the power button
 function togglePower() {
-    const powerButton = document.querySelector('button.power');
-    
-    // Toggle the 'active' class to switch between green and red states
-    powerButton.classList.toggle('active');
+  const powerButton = document.querySelector("button.power")
+  powerButton.classList.toggle("active")
+}
+
+function addMovie(button) {
+  const index = button.getAttribute("data-index")
+  const movieButton = document.createElement("button")
+  movieButton.textContent = `Movie ${index}`
+  movieButton.className = "movie" + index
+  movieButton.onclick = function () {
+    fetch(`/remote/launch/12/80${index}/series`)
   }
-  
-  // Attach the togglePower function to the power button's onclick event
-  function sendKey(key) {
-    if (key === 'Power') {
-      togglePower();
-    }
-    // You can add additional logic for other keys if needed
+  const buttonGrid = document.querySelector(".button-grid")
+  buttonGrid.replaceChild(movieButton, button)
+}
+
+function sendKey(key) {
+  if (key === "Power") {
+    togglePower()
   }
-  
+}
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,29 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Configure" })
 })
 
+/* GET Remote Set Up page. */
+router.get("/set_up_remote", function (req, res, next) {
+  res.render("set_up_remote", { title: "Remote Set Up" })
+})
+
+/* GET Pair page. */
+router.get("/pair", function (req, res, next) {
+  res.render("pair", { title: "Pair RemoteASK" })
+})
+
+/* GET Settings page. */
+router.get("/settings", function (req, res, next) {
+  res.render("settings", { title: "Settings" })
+})
+
+/* GET Help page. */
+router.get("/help", function (req, res, next) {
+  res.render("help", { title: "Help" })
+})
+
+/* GET About Us page. */
+router.get("/about_us", function (req, res, next) {
+  res.render("about_us", { title: "About Us" })
+})
+
 module.exports = router
