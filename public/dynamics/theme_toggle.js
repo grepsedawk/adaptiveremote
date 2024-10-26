9462d12e197b27069341f3a4565184485601e54d
José Alonso <jos.alonso@uc.cl>
2024-10-26 04:49:49 -0300

    views propportions tweaked, light/dark mode added

---
+++ public/dynamics/theme_toggle.js
@@ -0,0 +1,13 @@
+// theme_toggle.js
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
+
+themeToggle.addEventListener('click', () => {
+  if (body.dataset.theme === 'dark') {
+    body.dataset.theme = 'light';
+    themeToggle.innerHTML = '<i class="fas fa-sun"></i>';
+  } else {
+    body.dataset.theme = 'dark';
+    themeToggle.innerHTML = '<i class="fas fa-moon"></i>';
+  }
+});
