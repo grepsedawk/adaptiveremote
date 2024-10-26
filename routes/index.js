f45af21cebef1011f22fdf8d691acebefb39443c
José Alonso <jos.alonso@uc.cl>
2024-10-26 04:28:16 -0300

    Structure and styling of the page fixed

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,26 @@
 const express = require("express")
 const router = express.Router()
 
-/* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Configure" })
 })
 
-/* GET Remote Set Up page. */
 router.get("/set_up_remote", function (req, res, next) {
   res.render("set_up_remote", { title: "Remote Set Up" })
 })
 
-/* GET Pair page. */
 router.get("/pair", function (req, res, next) {
   res.render("pair", { title: "Pair RemoteASK" })
 })
 
-/* GET Settings page. */
 router.get("/settings", function (req, res, next) {
   res.render("settings", { title: "Settings" })
 })
 
-/* GET Help page. */
 router.get("/help", function (req, res, next) {
   res.render("help", { title: "Help" })
 })
 
-/* GET About Us page. */
 router.get("/about_us", function (req, res, next) {
   res.render("about_us", { title: "About Us" })
 })
