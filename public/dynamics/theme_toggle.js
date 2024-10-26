524dcb33e77f018e152f86a4e9ccaeafc206405d
José Alonso <jos.alonso@uc.cl>
2024-10-26 18:56:52 -0300

    Finally, the light/dark icon switching from moon to sun has been fixed

--- public/dynamics/theme_toggle.js
+++ public/dynamics/theme_toggle.js
@@ -1,21 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   const themeToggleButton = document.getElementById("theme-toggle");
+  const icon = themeToggleButton.querySelector("i");
   const currentTheme = localStorage.getItem("theme") || "light";
 
-  // Apply stored theme
+  // Apply the stored theme and set the initial icon based on the theme
   document.body.setAttribute("data-theme", currentTheme);
-
-  // Update icon based on theme
-  themeToggleButton.querySelector("i").className =
-    currentTheme === "dark" ? "fas fa-moon" : "fas fa-sun";
+  icon.classList.add(currentTheme === "dark" ? "fas": "fa-sun" , "fas", "fa-moon");
 
   themeToggleButton.addEventListener("click", () => {
-    const newTheme = document.body.getAttribute("data-theme") === "light" ? "dark" : "light";
+    const isLightMode = document.body.getAttribute("data-theme") === "light";
+    const newTheme = isLightMode ? "dark" : "light";
     document.body.setAttribute("data-theme", newTheme);
     localStorage.setItem("theme", newTheme);
 
-    // Update icon based on new theme
-    themeToggleButton.querySelector("i").className =
-      newTheme === "dark" ? "fas fa-moon" : "fas fa-sun";
+    // Update icon based on the new theme
+    icon.className = newTheme === "dark" ? "fas fa-sun" : "fas fa-moon";
   });
 });
