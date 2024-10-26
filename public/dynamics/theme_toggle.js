9896aa1b6ccd8e5d70ad2ed62605c204b26d1126
José Alonso <jos.alonso@uc.cl>
2024-10-26 04:54:57 -0300

    Problem with the sun and moon icons fixed

--- public/dynamics/theme_toggle.js
+++ public/dynamics/theme_toggle.js
@@ -5,9 +5,9 @@ const body = document.body;
 themeToggle.addEventListener('click', () => {
   if (body.dataset.theme === 'dark') {
     body.dataset.theme = 'light';
-    themeToggle.innerHTML = '<i class="fas fa-sun"></i>';
+    themeToggle.innerHTML = '<i class="fas fa-moon"></i>';
   } else {
     body.dataset.theme = 'dark';
-    themeToggle.innerHTML = '<i class="fas fa-moon"></i>';
+    themeToggle.innerHTML = '<i class="fas fa-sun"></i>';
   }
 });
